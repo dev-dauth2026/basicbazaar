b0cc806bbfff1ad66f2bb3a369a9e1d31555257c
Dauth Gurung <dauth22grg22@gmail.com>
2024-01-31 23:46:09 +1000

    order details functionality added

---
+++ public/order-details.php
@@ -0,0 +1,128 @@
+<?php 
+
+session_start();
+
+include '../includes/config.php'; 
+
+if(isset($_GET['logged_out'])){
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_name']);
+    unset($_SESSION['user_email']);
+    unset($_SESSION['logged_in']);
+
+    header('location: login.php?logged_out=You have been successfully logged out.');
+    exit();
+}
+
+if(!isset($_SESSION['logged_in'])){
+    header('location: login.php');
+}
+
+if(isset($_SESSION['logged_in'])){
+
+    if(isset($_GET['order_details']) && isset($_GET['order_id'])){
+        $user_id = $_SESSION['user_id'];
+        $order_id = $_GET['order_id'];
+
+
+        $stmt = $conn->prepare('SELECT * FROM order_items WHERE user_id=? && order_id=? ');
+        $stmt->bind_param('ii', $user_id,$order_id);
+        $stmt->execute();
+    
+        $order_details = $stmt->get_result();
+    }
+    
+}   
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Bazaar</title>
+
+    <?php include '../admin/includes/bootstrap_cdn.php'; ?>
+    <?php include '../admin/includes/fontawesome_cdn.php'; ?>
+
+    <!-- Your Custom CSS (if any) -->
+    <link rel="stylesheet" href="../assets/css/order_details/order_details.css">
+    <link rel="stylesheet" href="../assets/css/global_css/global.css">
+</head>
+<body> 
+
+
+       <!-- Include Navbar -->
+    <?php include '../includes/navbar.php'; ?>
+
+     <!-- Include category Navbar -->
+     <?php include '../includes/category_navbar.php'; ?>
+
+    
+    <main class="container p-5">
+        <h3 class="text-center">Orders Details</h3>
+        <hr class="col-2 mx-auto text-warning">
+        <?php if($order_details) { ?>
+        <table class="table table-borderless table-hover table-striped">
+            <thead>
+                <tr>
+                    <th class="bg-warning">Product Image</th>
+                    <th class="bg-warning">Order Id</th>
+                    <th class="bg-warning">Product Name</th>
+                    <th class="bg-warning">Product Price</th>
+                    <th class="bg-warning">Product Quantity</th>
+                    <th class="bg-warning ">Action</th>
+                </tr>
+                
+            </thead>
+            <body>
+                <?php while ($row= $order_details->fetch_assoc()) { ?>
+                <tr>
+                    <td>
+                        <div class="d-flex justify-content-center ">
+                                <div class="image-article d-flex justify-content-center">
+                                    <img class="order-images" src="../admin/product_images/<?php echo $row['product_image'] ?>" alt="<?php echo $row['product_name'] ?>">
+                            </div>   
+                        </div>
+                    </td>
+                    <td><?php echo $row['order_id'] ?> </td>
+                    <td><?php echo $row['product_name'] ?> </td>
+                    <td>$<?php echo $row['product_price'] ?> </td>
+                    <td><?php echo $row['product_quantity'] ?> </td>
+                    <td class="">
+                        <div class="d-flex gap-2 ">
+                            <!-- <form action="product-details.php" method="GET">
+                                <input type="hidden" name="product_details" value="<?php echo $row['product_id'] ?>">
+                                <input type="hidden" name="order_id" value="<?php echo $row['order_id'] ?>">
+                                <button title="view order details" class="border border-0 bg-transparent" name="product_details" type="submit"><i class="far fa-eye"></i></button>
+                            </form> -->
+                            <a href="product-details.php?product_details=<?php echo $row['product_id'] ?>" title="view order details" class="text-secondary border border-0 bg-transparent" name="product_details" type="submit"><i class="far fa-eye"></i></a>
+                            <form action="" method="GET">
+                                    <button title="cancel the order" class="border border-0 bg-transparent" name="cancel_order" type="submit"><i class="fas fa-trash-alt"></i></button>
+                            </form>
+                        </div>
+                        
+                        
+                    </td>
+
+
+                </tr>
+                <?php } ?>
+            </body>
+
+        </table>
+        <?php } ?> 
+    </main>    
+
+    <!-- Include Navbar -->
+     <?php include '../includes/footer.php'; ?>
+
+    <!-- Bootstrap JS (optional, but may be needed for some components) -->
+    <?php include '../admin/includes/js_cdn.php'; ?>
+
+
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ public/order-product-detail.php
@@ -0,0 +1,100 @@
+<?php 
+
+session_start();
+
+include '../includes/config.php'; 
+
+if(isset($_GET['logged_out'])){
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_name']);
+    unset($_SESSION['user_email']);
+    unset($_SESSION['logged_in']);
+
+    header('location: login.php?logged_out=You have been successfully logged out.');
+    exit();
+}
+
+if(!isset($_SESSION['logged_in'])){
+    header('location: login.php');
+}
+
+if(isset($_SESSION['logged_in'])){
+
+    if(isset($_POST['order_product_detail']) && isset($_POST['product_id']) && isset($_POST['order_id'])){
+        $user_id = $_SESSION['user_id'];
+        $order_id = $_POST['order_id'];
+        $product_id = $_POST['product_id'];
+
+
+        $stmt = $conn->prepare('SELECT * FROM order_items WHERE user_id=? AND order_id=? AND product_id=?');
+        $stmt->bind_param('iii', $user_id,$order_id, $product_id);
+        $stmt->execute();
+    
+        $order_product_detail = $stmt->get_result();
+    }
+    
+}   
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Bazaar</title>
+
+    <?php include '../admin/includes/bootstrap_cdn.php'; ?>
+    <?php include '../admin/includes/fontawesome_cdn.php'; ?>
+
+    <!-- Your Custom CSS (if any) -->
+    <link rel="stylesheet" href="../assets/css/order-product-detail/order-product-detail.css">
+    <link rel="stylesheet" href="../assets/css/global_css/global.css">
+</head>
+<body> 
+
+
+       <!-- Include Navbar -->
+    <?php include '../includes/navbar.php'; ?>
+
+     <!-- Include category Navbar -->
+     <?php include '../includes/category_navbar.php'; ?>
+
+    
+    <main class="container p-5 ">
+        <h3 class="text-center">Orders Details</h3>
+            <hr class="col-2 mx-auto text-warning">
+        <div class="row  ">
+            
+            <?php while ($row= $order_product_detail->fetch_assoc()) { ?>
+            <div class="col-6 d-flex justify-content-center ">
+                <div class="order-product-detail d-flex justify-content-center">
+                    <img class="order-product-images" src="../admin/product_images/<?php echo $row['product_image'] ?>" alt="<?php echo $row['product_name'] ?>">
+                </div>   
+            </div>
+            <div class="col-6" >
+                <h3><?php echo $row['product_name'] ?> </h3>
+                <p>Ratings: </p>
+                <p>$<?php echo $row['product_price'] ?> </p>
+                <p>Quantity: <?php echo $row['product_quantity'] ?> </p>
+    
+            </div>
+            <?php } ?>
+            
+
+        </div>
+
+    </main>    
+
+
+    <!-- Include Navbar -->
+     <?php include '../includes/footer.php'; ?>
+
+    <!-- Bootstrap JS (optional, but may be needed for some components) -->
+    <?php include '../admin/includes/js_cdn.php'; ?>
+
+
+    <script src="js/script.js"></script>
+</body>
+</html>
