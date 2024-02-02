ccbd382b3f5e952dcbe6d1f41c029681451b6b2b
Dauth Gurung <dauth22grg22@gmail.com>
2024-02-03 00:13:50 +1000

    edit account functionality created

--- public/edit-account.php
+++ public/edit-account.php
@@ -4,6 +4,10 @@ session_start();
 
 include '../includes/config.php'; 
 
+if(!isset($_SESSION['logged_in'])){
+    header('location: login.php');
+}
+
 if(isset($_GET['logged_out'])){
     unset($_SESSION['user_id']);
     unset($_SESSION['user_name']);
@@ -14,13 +18,36 @@ if(isset($_GET['logged_out'])){
     exit();
 }
 
-if(!isset($_SESSION['logged_in'])){
-    header('location: login.php');
+if(isset($_SESSION['logged_in'])){
+    $user_id= $_SESSION['user_id'];
+
+    if(isset($_POST['update_user_profile'])){
+        $user_name= $_POST['username'];
+        $user_phone = $_POST['phone'];
+        $user_email= $_POST['email'];
+        $user_address = $_POST['address'];
+        $user_city = $_POST['city'];
+        $user_state = $_POST['state'];
+        $user_postcode = $_POST ['postcode'];
+        $user_country = $_POST['country'];
+
+        $stmt = $conn->prepare("UPDATE users SET user_name = ?, user_phone =?, user_email=?, user_address= ?, user_city =?, user_state = ?, user_postcode=?, user_country =? 
+                    WHERE user_id = ?");
+        $stmt->bind_param("ssssssisi",$user_name,$user_phone,$user_email,$user_address,$user_city,$user_state,$user_postcode,$user_country, $user_id);
+        $stmt->execute();
+        header('location: edit-account.php?account_update = Account has been updated successfully.');
+    
+    }
+
+    
+    $stmt = $conn->prepare('SELECT * FROM users WHERE user_id = ? LIMIT 1' );
+    $stmt->bind_param('i', $user_id);
+    $stmt->execute();
+    $user_result = $stmt->get_result();
+
+   
+
 }
-
-
-
-
 
 ?>
 
@@ -51,6 +78,13 @@ if(!isset($_SESSION['logged_in'])){
     
     <main class="container-fluid ">
         <div class="row  p-5 ">
+
+            
+        <?php  if(isset($_GET['account_update'])){?>
+                <p class="p-2 px-3 bg-success-subtle text-success"><?php echo $_GET['account_update']; ?> </p>
+           <?php } ?>
+            
+            
             <!-- -navbar  -->
             <?php include '../includes/user_account_navbar.php' ?>
 
@@ -84,46 +118,52 @@ if(!isset($_SESSION['logged_in'])){
                     <div class="d-flex flex-column">
                         <h5 class="text-secondary">Personal Information</h5>
                         <hr class="col-2  text-warning">
-                        <form class="d-flex flex-column gap-3">
-                                <div class="d-flex justify-content-between gap-3">
-                                    <div class="form-group col-5 d-flex flex-column gap-2">
-                                        <label for="username">Username:</label>
-                                        <input class="form-control " name="username" type="text" value="" required>
-                                    </div>
-                                    <div class="form-group col-5 d-flex flex-column gap-2">
-                                        <label for="phone">Phone:</label>
-                                        <input class="form-control " name="phone" type="tel" pattern="[0-9]{10}" value="" required>
-                                    </div>
-                                </div>
-                                
-                                <div class="form-group d-flex flex-column gap-2">
-                                    <label for="email">Email:</label>
-                                    <input class="form-control " name="email" type="email"  value="" required>
-                                </div>
-                                <div class="form-group d-flex flex-column gap-2">
-                                    <label for="address">Address:</label>
-                                    <input class="form-control " name="address" type="text"  value="" required>
-                                </div>
-                                <div class="d-flex gap-3">
-                                    <div class="form-group col-3 d-flex flex-column gap-2">
-                                        <label for="city">City:</label>
-                                        <input class="form-control " name="city" type="text"  value="" required>
-                                    </div>
-                                    <div class="form-group col-3 d-flex flex-column gap-2">
-                                        <label for="state">State:</label>
-                                        <input class="form-control " name="state" type="text"  value="" required>
-                                    </div>
-                                    <div class="form-group d-flex flex-column gap-2">
-                                        <label for="postcode">Post Code:</label>
-                                        <input class="form-control " name="postcode" type="text"  value="" required>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-end">
-                                    <input class="btn border-warning text-warning" type="submit" name="update_user_profile" value="Save">
-                                </div>
-                                
+                        <form class="d-flex flex-column gap-3" method="POST" action="edit-account.php">
+                                <?php while ($row = $user_result->fetch_assoc()){?>
+                                        <div class="d-flex justify-content-between gap-3">
+                                            <div class="form-group col-5 d-flex flex-column gap-2">
+                                                <label class="text-secondary" for="username">Username:</label>
+                                                <input class="form-control " name="username" type="text" value="<?php echo $row['user_name']?>" required >
+                                            </div>
+                                            <div class="form-group col-5 d-flex flex-column gap-2">
+                                                <label class="text-secondary"  for="phone">Phone:</label>
+                                                <input class="form-control " name="phone" type="tel" pattern="[0-9]{10}" value="<?php echo $row['user_phone']?>" required>
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="form-group d-flex flex-column gap-2">
+                                            <label class="text-secondary" for="email">Email:</label>
+                                            <input class="form-control " name="email" type="email"  value="<?php echo $row['user_email']?>" required>
+                                        </div>
+                                        <div class="form-group d-flex flex-column gap-2">
+                                            <label class="text-secondary" for="address">Address:</label>
+                                            <input class="form-control " name="address" type="text"  value="<?php echo $row['user_address']?>" >
+                                        </div>
+                                        <div class="d-flex gap-3">
+                                            <div class="form-group col-3 d-flex flex-column gap-2">
+                                                <label class="text-secondary" for="city">City:</label>
+                                                <input class="form-control " name="city" type="text"  value="<?php echo $row['user_city']?>" >
+                                            </div>
+                                            <div class="form-group col-3 d-flex flex-column gap-2">
+                                                <label class="text-secondary" for="state">State:</label>
+                                                <input class="form-control " name="state" type="text"  value="<?php echo $row['user_state']?>" required>
+                                            </div>
+                                            <div class="form-group d-flex flex-column gap-2">
+                                                <label class="text-secondary" for="postcode">Post Code:</label>
+                                                <input class="form-control " name="postcode" type="text"  value="<?php echo $row['user_postcode']?>" required>
+                                            </div>
+                                            <div class="form-group d-flex flex-column gap-2">
+                                                <label class="text-secondary" for="country">Country:</label>
+                                                <input class="form-control " name="country" type="text"  value="<?php echo $row['user_country']?>" required>
+                                            </div>
+                                        </div>
+                                    <?php } ?>
+                                        <div class="d-flex justify-content-end">
+                                            <input class="btn border-warning text-warning" type="submit" name="update_user_profile" value="Save">
+                                        </div>
+                                        
 
-                    </form>
+                            </form>
                     </div>
                 </section>
 
